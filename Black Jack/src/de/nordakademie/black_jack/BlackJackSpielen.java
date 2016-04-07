249c2e5c2e802ff5389cb381df089328b40f4149
natascha <natascha.voelkl@nordakademie.de>
2016-04-07 18:19:51 +0200

    Fertiges Grundspiel!

--- Black Jack/src/de/nordakademie/black_jack/BlackJackSpielen.java
+++ Black Jack/src/de/nordakademie/black_jack/BlackJackSpielen.java
@@ -17,10 +17,34 @@ public class BlackJackSpielen {
 		ohrqualle.karteAufnehmen(stapelA.gibKarte());
 		// erste Runde Kartenausgabe
 		spielStand();
+
+		while (natascha.punktezaehler() < 21 && natascha.karteZiehen() == true) {
+			natascha.karteAufnehmen(stapelA.gibKarte());
+		}
+		// weitere Runden Spieler zieht
+		while (ohrqualle.punktezaehler() < 17) {
+			ohrqualle.karteAufnehmen(stapelA.gibKarte());
+		}
+		// Geber zieht
+		spielStand();
+		//Spielstand ausgeben.
+		if (natascha.punktezaehler() < 22) {
+			if (ohrqualle.punktezaehler() < natascha.punktezaehler()) {
+				System.out.println("Du bist der Babbo!");
+			}
+			if (ohrqualle.punktezaehler() == natascha.punktezaehler()) {
+				System.out.println("Chabos wissen nicht soo genau, wer der Babbo ist...");
+			} else {
+				System.out.println("Du bist der Chabo...");
+			}
+		} else {
+			System.out.println("Du bist der alpha Chabo...");
+		}//Wer gewinnt
 	}
 
 	private void spielStand() {
 		System.out.println("Der Geber hat " + ohrqualle.punktezaehler() + " Punkte.");
 		System.out.println("Der Spieler hat " + natascha.punktezaehler() + " Punkte.");
+		//Methode zur Ausgabe des Spielstand
 	}
 }
